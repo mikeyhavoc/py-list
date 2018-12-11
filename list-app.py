9e88a9a89cc5b53607c48ac6e5e0204647ab6235
mikey <manicdesigner@gmail.com>
2018-12-11 11:24:37 -0500

    add author info, add welcome_prompt() function -> listing commands and greeting. add help_screen() function -> listing help commands.

---
+++ list-app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+# App: List application
+# author: Michael Williams
+# Date: 2018-12-11
+# version: 1.0.0
+
+
+def welcome_prompt():
+    print('*' * 30)
+    print('Welcome to the List app')
+    print('1. when ready you can enter an item.')
+    print('2. Anytime you need help just type "help"')
+    print('3. if you want to see the list at any time Enter "listing".')
+    print('4. if you want to "delete" an item, enter "delete [item name]"')
+    print('5. once done, enter "done" and we will print out your list.')
+    print('Happy Listing :)')
+    print('*' * 30)
+
+def help_screen():
+    print('*' * 25)
+    print('Help File')
+    print('1. when ready you can enter an item.')
+    print('2. Anytime you need help just type "help"')
+    print('3. if you want to see the list at any time Enter "listing".')
+    print('4. if you want to "delete" an item, enter "delete [item name]"')
+    print('5. once done, enter "done" and we will print out your list.')
+    print('*' * 25)
+
