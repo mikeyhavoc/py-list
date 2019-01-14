c3033e48ac5f21c8ae56a5e11f241cb8231a4c3b
mikey <manicdesigner@gmail.com>
2019-01-14 12:37:32 -0500

    add 'item_list' function to show list and other functions.

--- list-app.py
+++ list-app.py
@@ -17,13 +17,54 @@ def welcome_prompt():
     print('Happy Listing :)')
     print('*' * 30)
 
+
 def help_screen():
     print('*' * 25)
     print('Help File')
     print('1. when ready you can enter an item.')
     print('2. Anytime you need help just type "help"')
-    print('3. if you want to see the list at any time Enter "listing".')
+    print('3. if you want to see the list at any time Enter "list".')
     print('4. if you want to "delete" an item, enter "delete [item name]"')
     print('5. once done, enter "done" and we will print out your list.')
     print('*' * 25)
 
+
+def item_list(lists):
+    listing = lists.copy() # prints out full list.
+    for item in listing:
+        print(item)
+
+
+def list_count(items):
+    count = len(items)
+    return count
+
+
+def list_complete(items):
+    listing = items.copy()
+    for num, item in enumerate(listing):
+        print('{}# {}'.format(num + 1, item))
+
+
+def master():
+    items = list()
+    welcome_prompt()
+    while True:
+        new_item = input('add > ').lower()
+        if new_item == 'help':  # HELP | help info given.
+            help_screen()
+        elif new_item == 'list':  # LIST | serves listing so far.
+            item_list(items)
+            continue
+        elif new_item == 'done':  # COMPLETE | serves number of items and items.
+            list_complete(items)
+            break
+
+        else:
+            items.append(new_item)
+            num_items = list_count(items)
+            print('you have {} items so far.'.format(num_items))
+
+
+if __name__ == '__main__':
+    master()
